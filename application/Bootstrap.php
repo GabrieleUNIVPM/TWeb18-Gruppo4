281e19490499865365ad53aab9aefa44b3a93e2b
EMANUELE <EMANUELE@Emanuele>
2018-05-19 15:31:06 +0200

    messa barra di navigazione in file esterno e aggiunto href contattaci

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,9 +3,6 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
     protected function _initRequest()
-	// Aggiunge un'istanza di Zend_Controller_Request_Http nel Front_Controller
-	// che permette di utilizzare l'helper baseUrl() nel Bootstrap.php
-	// Necessario solo se la Document-root di Apache non è la cartella public/
     {
         $this->bootstrap('FrontController');
         $front = $this->getResource('FrontController');
