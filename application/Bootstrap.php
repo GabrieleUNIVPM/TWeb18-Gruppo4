e8ff8c3e44c3c351229c0549118e8786ac9eb137
Gabriele <g.debartolomeo09@gmail.com>
2018-06-04 11:27:27 +0200

    Implementato collegamento al DB da Bootstrap.php anziché da application.ini, come indicato dal prof nella checklist finale del progetto

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -24,6 +24,20 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $this->getResourceLoader()
              ->addResourceType('modelResource','models/resources','Resource');
   	}
+        
+    	protected function _initDbParms()
+    {
+    	include_once (APPLICATION_PATH . '/../../include/connect.php');
+		$db = new Zend_Db_Adapter_Pdo_Mysql(array(
+    			'host'     => $HOST,
+    			'username' => $USER,
+    			'password' => $PASSWORD,
+    			'dbname'   => $DB
+				));  
+		Zend_Db_Table_Abstract::setDefaultAdapter($db);
+                //metodo che si può utilizzare direttamente sulla classe dell'oggetto
+                //prende come parametro un adapter
+	}
 
 }
 
